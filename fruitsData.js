d26a524724f189da346a324092010b3d19e332a8
Kristoff-Villanueva <kristoffer.villanueva18@gmail.com>
2022-10-17 23:09:46 +0800

    first commit

---
+++ fruitsData.js
@@ -0,0 +1,10 @@
+let fruits = [
+	{ fruit: "Mango", Price: 120 },
+	{ fruit: "Banana", Price: 79 },
+	{ fruit: "Pineapple", Price: 60 },
+	{ fruit: "Lemon", Price: 90 },
+	{ fruit: "Apple", Price: 50 },
+	{ fruit: "Orange", Price: 45 },
+];
+
+export default fruits;
