efa1473d0cc3aceaf822b17dc3b5ecf02e9b8301
Kristoff-Villanueva <kristoffer.villanueva18@gmail.com>
2022-10-19 23:21:01 +0800

    added wallet and points

--- fruitsData.js
+++ fruitsData.js
@@ -7,6 +7,15 @@ const fruits = [
 	{ fruit: "Orange", price: 45, image: "./images/fruits/orange.png", reviews: '⭐⭐⭐⭐' },
 ];
 
+const vegetables = [
+	{ fruit: "Mango", price: 120, image: "./images/fruits/mango.png", reviews: '⭐⭐⭐⭐' },
+	{ fruit: "Banana", price: 79, image: "./images/fruits/banana.png", reviews: '⭐⭐⭐⭐⭐' },
+	{ fruit: "Pineapple", price: 60, image: "./images/fruits/pineapple.png", reviews: '⭐⭐⭐' },
+	{ fruit: "Lemon", price: 90, image: "./images/fruits/lemon.png", reviews: '⭐⭐⭐⭐⭐' },
+	{ fruit: "Apple", price: 50, image: "./images/fruits/apple.png", reviews: '⭐⭐⭐' },
+	{ fruit: "Orange", price: 45, image: "./images/fruits/orange.png", reviews: '⭐⭐⭐⭐' },
+];
+
 const categories = [
 	{categoryName: "Fruits", image:"./fruits.jpg"},
 	{categoryName: "Vegetable", image:"./vegetables.jpg"},
